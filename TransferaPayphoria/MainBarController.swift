86c036677a83fa81cadef990852aa900f0f73b32
emiliamam <emilka20052015@icloud.com>
2024-07-28 16:51:32 +0300

    Initial Commit

---
+++ TransferaPayphoria/MainBarController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class MainBarController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Скрыть навигационную панель
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+
+    }
+}
